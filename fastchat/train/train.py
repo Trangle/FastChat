ef40ea3ef9eb6a5aeb1e97cebae592b4732cd6c8
Trangle <kw_w@foxmail.com>
2023-07-15 16:23:28 +0000

    adjust train struct and logic

--- fastchat/train/train.py
+++ fastchat/train/train.py
@@ -17,8 +17,10 @@
 import copy
 from dataclasses import dataclass, field
 import json
+import jsonlines
 import pathlib
-from typing import Dict, Optional, Sequence
+from multiprocessing import Pool
+from typing import Dict, Optional, Sequence, List, Tuple
 
 import numpy as np
 import torch
@@ -76,94 +78,121 @@ def safe_save_model_for_hf_trainer(trainer: transformers.Trainer, output_dir: st
         trainer._save(output_dir, state_dict=cpu_state_dict)  # noqa
 
 
-def preprocess(
-    sources,
-    tokenizer: transformers.PreTrainedTokenizer,
-) -> Dict:
-    conv = get_conversation_template("vicuna")
-    roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
+class Preprocessor(object):
+    def __init__(self, tokenizer: transformers.PreTrainedTokenizer):
+        self.tokenizer = tokenizer
+        self.conv = get_conversation_template("vicuna")
+        assert self.conv.sep_style == SeparatorStyle.ADD_COLON_TWO
 
-    # Apply prompt templates
-    conversations = []
-    for i, source in enumerate(sources):
-        if roles[source[0]["from"]] != conv.roles[0]:
-            # Skip the first one if it is not from human
-            source = source[1:]
+        self.roles = {"human": self.conv.roles[0], "gpt": self.conv.roles[1]}
+        self.turn_sep = self.conv.sep + self.roles.get("gpt") + ": "
 
-        conv.messages = []
-        for j, sentence in enumerate(source):
-            role = roles[sentence["from"]]
-            assert role == conv.roles[j % 2], f"{i}"
-            conv.append_message(role, sentence["value"])
-        conversations.append(conv.get_prompt())
+    def apply_prompt_template(self, sources, systems=None) -> List[str]:
+        conversations = []
+        for i, source in enumerate(sources):
+            if source[0]["from"] != "human":
+                source = source[1:]
 
-    # Tokenize conversations
-    input_ids = tokenizer(
-        conversations,
-        return_tensors="pt",
-        padding="max_length",
-        max_length=tokenizer.model_max_length,
-        truncation=True,
-    ).input_ids
-    targets = input_ids.clone()
+            self.conv.messages = []
+            for j, sentence in enumerate(source):
+                role = self.roles.get(sentence["from"])
+                assert role == self.conv.roles[j % 2], f"{i}"
+                self.conv.append_message(role, sentence["value"])
+            if systems and systems[i]:
+                self.conv.system = systems[i]
+            prompt = self.conv.get_prompt()
+            conversations.append(prompt)
+        return conversations
 
-    assert conv.sep_style == SeparatorStyle.ADD_COLON_TWO
+    def tokenize_conversations(self, conversations) -> Tuple[List, List]:
+        input_ids = self.tokenizer(
+            conversations,
+            return_tensors="pt",
+            padding="max_length",
+            max_length=self.tokenizer.model_max_length,
+            truncation=True,
+        ).input_ids
+        targets = input_ids.clone()
+        return input_ids, targets
 
-    # Mask targets. Only compute loss on the assistant outputs.
-    sep = conv.sep + conv.roles[1] + ": "
-    for conversation, target in zip(conversations, targets):
-        total_len = int(target.ne(tokenizer.pad_token_id).sum())
+    def mask_targets(self, conversations, targets) -> List[int]:
+        for conversation, target in zip(conversations, targets):
+            total_len = int(target.ne(self.tokenizer.pad_token_id).sum())
 
-        turns = conversation.split(conv.sep2)
-        cur_len = 1
-        target[:cur_len] = IGNORE_TOKEN_ID
-        for i, turn in enumerate(turns):
-            if turn == "":
-                break
-            turn_len = len(tokenizer(turn).input_ids)
+            turns = conversation.split(self.conv.sep2)
+            cur_len = 1
+            target[:cur_len] = IGNORE_TOKEN_ID
+            for i, turn in enumerate(turns):
+                if turn == "":
+                    break
+                turn_len = len(self.tokenizer(turn).input_ids)
 
-            parts = turn.split(sep)
-            if len(parts) != 2:
-                break
-            parts[0] += sep
-            # "-2" is hardcoded for the LLaMA tokenizer to make the offset correct.
-            instruction_len = len(tokenizer(parts[0]).input_ids) - 2
+                parts = turn.split(self.turn_sep)
+                if len(parts) != 2:
+                    break
+                parts[0] += self.turn_sep
 
-            # Ignore the user instructions
-            target[cur_len : cur_len + instruction_len] = IGNORE_TOKEN_ID
-            cur_len += turn_len
+                # "-2" is hardcoded for the LLaMA tokenizer to make the offset correct.
+                instruction_len = len(self.tokenizer(parts[0]).input_ids) - 2
 
-        target[cur_len:] = IGNORE_TOKEN_ID
+                target[cur_len : cur_len + instruction_len] = IGNORE_TOKEN_ID
+                cur_len += turn_len
 
-        if False:  # Inspect and check the correctness of masking
-            z = target.clone()
-            z = torch.where(z == IGNORE_TOKEN_ID, tokenizer.unk_token_id, z)
-            rank0_print(tokenizer.decode(z))
+            target[cur_len:] = IGNORE_TOKEN_ID
 
-        if cur_len < tokenizer.model_max_length:
-            if cur_len != total_len:
-                target[:] = IGNORE_TOKEN_ID
-                rank0_print(
-                    f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
-                    f" (ignored)"
-                )
+            if False:  # Inspect and check the correctness of masking
+                z = target.clone()
+                z = torch.where(z == IGNORE_TOKEN_ID, self.tokenizer.unk_token_id, z)
+                rank0_print(self.tokenizer.decode(z))
 
-    return dict(
-        input_ids=input_ids,
-        labels=targets,
-        attention_mask=input_ids.ne(tokenizer.pad_token_id),
-    )
+            if cur_len < self.tokenizer.model_max_length:
+                if cur_len != total_len:
+                    target[:] = IGNORE_TOKEN_ID
+                    rank0_print(
+                        f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+                        f" (ignored)"
+                    )
+        return targets
+
+    def preprocess(self, sources, **kwargs) -> Dict:
+        systems = None if not kwargs else kwargs.get("systems", None)
+
+        # If the data volume is small, process it directly in the main thread
+        if len(sources) <= 1000:
+            conversations = self.apply_prompt_template(sources, systems)
+            input_ids, targets = self.tokenize_conversations(conversations)
+            targets = self.mask_targets(conversations, targets)
+        else:  # If the data volume is large, use multithreading for processing
+            with Pool() as p:
+                conversations = p.apply_async(
+                    self.apply_prompt_template, (sources, systems)
+                ).get()
+                input_ids, targets = p.apply_async(
+                    self.tokenize_conversations, (conversations)
+                ).get()
+                targets = p.apply_async(
+                    self.mask_targets, (conversations, targets)
+                ).get()
+                p.close()
+                p.join()
+
+        return dict(
+            input_ids=input_ids,
+            labels=targets,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
 
 
 class SupervisedDataset(Dataset):
     """Dataset for supervised fine-tuning."""
 
-    def __init__(self, raw_data, tokenizer: transformers.PreTrainedTokenizer):
+    def __init__(self, raw_data, preprocessor: Preprocessor):
         super(SupervisedDataset, self).__init__()
 
         rank0_print("Formatting inputs...")
+        systems = [example.get("system", "") for example in raw_data]
         sources = [example["conversations"] for example in raw_data]
-        data_dict = preprocess(sources, tokenizer)
+        data_dict = preprocessor.preprocess(sources, systems=systems)
 
         self.input_ids = data_dict["input_ids"]
         self.labels = data_dict["labels"]
@@ -183,12 +212,11 @@ class SupervisedDataset(Dataset):
 class LazySupervisedDataset(Dataset):
     """Dataset for supervised fine-tuning."""
 
-    def __init__(self, raw_data, tokenizer: transformers.PreTrainedTokenizer):
+    def __init__(self, raw_data, preprocessor: Preprocessor):
         super(LazySupervisedDataset, self).__init__()
-        self.tokenizer = tokenizer
+        self.preprocessor = preprocessor
 
         rank0_print("Formatting inputs...Skip in lazy mode")
-        self.tokenizer = tokenizer
         self.raw_data = raw_data
         self.cached_data_dict = {}
 
@@ -199,7 +227,10 @@ class LazySupervisedDataset(Dataset):
         if i in self.cached_data_dict:
             return self.cached_data_dict[i]
 
-        ret = preprocess([self.raw_data[i]["conversations"]], self.tokenizer)
+        ret = self.preprocessor.preprocess(
+            [self.raw_data[i]["conversations"]],
+            systems=[self.raw_data[i].get("system", "")],
+        )
         ret = dict(
             input_ids=ret["input_ids"][0],
             labels=ret["labels"][0],
@@ -211,27 +242,37 @@ class LazySupervisedDataset(Dataset):
 
 
 def make_supervised_data_module(
-    tokenizer: transformers.PreTrainedTokenizer, data_args
+    preprocessor: Preprocessor, data_args, train_ratio=0.98
 ) -> Dict:
     """Make dataset and collator for supervised fine-tuning."""
+    train_ratio = min(train_ratio, 1.0)
     dataset_cls = (
         LazySupervisedDataset if data_args.lazy_preprocess else SupervisedDataset
     )
     rank0_print("Loading data...")
-    raw_data = json.load(open(data_args.data_path, "r"))
+    data_path = data_args.data_path
+    if data_path.endswith(".json"):
+        raw_data = json.load(open(data_path, "r"))
+    elif data_path.endswith(".jsonl"):
+        with jsonlines.open(data_path, mode="r") as reader:
+            raw_data = [item for item in reader]
 
     # Split train/test
     np.random.seed(0)
     perm = np.random.permutation(len(raw_data))
-    split = int(len(perm) * 0.98)
+    split = int(len(perm) * train_ratio)
     train_indices = perm[:split]
-    eval_indices = perm[split:]
+    if train_ratio < 1:
+        eval_indices = perm[split:]
+    else:
+        # if train_ratio==1, we use 5% of data as eval data, make sure trainer will not throw error when eval data is empty
+        eval_indices = perm[-int(len(perm) * 0.05) :]
     train_raw_data = [raw_data[i] for i in train_indices]
     eval_raw_data = [raw_data[i] for i in eval_indices]
     rank0_print(f"#train {len(train_raw_data)}, #eval {len(eval_raw_data)}")
 
-    train_dataset = dataset_cls(train_raw_data, tokenizer=tokenizer)
-    eval_dataset = dataset_cls(eval_raw_data, tokenizer=tokenizer)
+    train_dataset = dataset_cls(train_raw_data, preprocessor=preprocessor)
+    eval_dataset = dataset_cls(eval_raw_data, preprocessor=preprocessor)
     return dict(train_dataset=train_dataset, eval_dataset=eval_dataset)
 
 
@@ -257,7 +298,10 @@ def train():
     )
     tokenizer.pad_token = tokenizer.unk_token
 
-    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+    preprocessor = Preprocessor(tokenizer)
+    data_module = make_supervised_data_module(
+        preprocessor=preprocessor, data_args=data_args
+    )
     trainer = Trainer(
         model=model, tokenizer=tokenizer, args=training_args, **data_module
     )
--- fastchat/train/train_baichuan.py
+++ fastchat/train/train_baichuan.py
@@ -14,262 +14,71 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from dataclasses import dataclass, field
-import json
-import jsonlines
 import pathlib
 from multiprocessing import Pool
-from typing import Dict, Optional, Sequence
+from typing import List
 
-import numpy as np
 import torch
-from torch.utils.data import Dataset
 import transformers
 from transformers import Trainer
 from transformers.trainer_pt_utils import LabelSmoother
 
-from fastchat.conversation import SeparatorStyle
-from fastchat.model.model_adapter import get_conversation_template
+from fastchat.train.train import (
+    DataArguments,
+    ModelArguments,
+    TrainingArguments,
+    Preprocessor,
+    make_supervised_data_module,
+    rank0_print,
+    safe_save_model_for_hf_trainer,
+)
 
 IGNORE_TOKEN_ID = LabelSmoother.ignore_index
-
-
-@dataclass
-class ModelArguments:
-    model_name_or_path: Optional[str] = field(default="facebook/opt-125m")
-
-
-@dataclass
-class DataArguments:
-    data_path: str = field(
-        default=None, metadata={"help": "Path to the training data."}
-    )
-    lazy_preprocess: bool = False
-
-
-@dataclass
-class TrainingArguments(transformers.TrainingArguments):
-    cache_dir: Optional[str] = field(default=None)
-    optim: str = field(default="adamw_torch")
-    model_max_length: int = field(
-        default=512,
-        metadata={
-            "help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."
-        },
-    )
-
 
 local_rank = None
 
 
-def rank0_print(*args):
-    if local_rank == 0:
-        print(*args)
+class BaichuanPreprocessor(Preprocessor):
+    def __init__(self, tokenizer: transformers.PreTrainedTokenizer):
+        super(BaichuanPreprocessor, self).__init__(tokenizer)
 
+    def mask_targets(self, conversations, targets) -> List[int]:
+        for conversation, target in zip(conversations, targets):
+            total_len = int(target.ne(self.tokenizer.pad_token_id).sum())
 
-def safe_save_model_for_hf_trainer(trainer: transformers.Trainer, output_dir: str):
-    """Collects the state dict and dump to disk."""
-    state_dict = trainer.model.state_dict()
-    if trainer.args.should_save:
-        cpu_state_dict = {key: value.cpu() for key, value in state_dict.items()}
-        del state_dict
-        trainer._save(output_dir, state_dict=cpu_state_dict)  # noqa
+            turns = conversation.split(self.conv.sep2)
+            cur_len = 0
+            for i, turn in enumerate(turns):
+                if turn == "":
+                    break
+                turn_len = len(self.tokenizer(turn + self.conv.sep2).input_ids)
 
+                parts = turn.split(self.turn_sep)
+                if len(parts) != 2:
+                    break
+                parts[0] += self.turn_sep
 
-def apply_prompt_template(sources, systems=None):
-    conv = get_conversation_template("vicuna")
-    roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
-    conversations = []
-    for i, source in enumerate(sources):
-        if roles[source[0]["from"]] != conv.roles[0]:
-            source = source[1:]
+                # "-1" is hardcoded for the Baichuan tokenizer to make the offset correct.
+                instruction_len = len(self.tokenizer(parts[0]).input_ids) - 1
 
-        conv.messages = []
-        for j, sentence in enumerate(source):
-            role = roles[sentence["from"]]
-            assert role == conv.roles[j % 2], f"{i}"
-            conv.append_message(role, sentence["value"])
-        if systems and systems[i]:
-            conv.system = systems[i]
-        prompt = conv.get_prompt()
-        conversations.append(prompt)
-    return conversations, conv
+                target[cur_len : cur_len + instruction_len] = IGNORE_TOKEN_ID
+                cur_len += turn_len
 
+            target[cur_len:] = IGNORE_TOKEN_ID
 
-def tokenize_conversations(conversations, tokenizer):
-    input_ids = tokenizer(
-        conversations,
-        return_tensors="pt",
-        padding="max_length",
-        max_length=tokenizer.model_max_length,
-        truncation=True,
-    ).input_ids
-    targets = input_ids.clone()
-    return input_ids, targets
+            if False:  # Inspect and check the correctness of masking
+                z = target.clone()
+                z = torch.where(z == IGNORE_TOKEN_ID, self.tokenizer.unk_token_id, z)
+                rank0_print(self.tokenizer.decode(z))
 
-
-def mask_targets(conversations, targets, tokenizer, conv):
-    sep = conv.sep + conv.roles[1] + ": "
-    for conversation, target in zip(conversations, targets):
-        total_len = int(target.ne(tokenizer.pad_token_id).sum())
-
-        turns = conversation.split(conv.sep2)
-        cur_len = 0
-        target[:cur_len] = IGNORE_TOKEN_ID
-        for i, turn in enumerate(turns):
-            if turn == "":
-                break
-            turn_len = len(tokenizer(turn + conv.sep2).input_ids)
-
-            parts = turn.split(sep)
-            if len(parts) != 2:
-                break
-            parts[0] += sep
-            instruction_len = len(tokenizer(parts[0]).input_ids) - 1
-
-            target[cur_len : cur_len + instruction_len] = IGNORE_TOKEN_ID
-            cur_len += turn_len
-
-        target[cur_len:] = IGNORE_TOKEN_ID
-
-        if False:  # Inspect and check the correctness of masking
-            z = target.clone()
-            z = torch.where(z == IGNORE_TOKEN_ID, tokenizer.unk_token_id, z)
-            rank0_print(tokenizer.decode(z))
-
-        if cur_len < tokenizer.model_max_length:
-            if cur_len != total_len:
-                target[:] = IGNORE_TOKEN_ID
-                rank0_print(
-                    f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
-                    f" (ignored)"
-                )
-    return targets
-
-
-def preprocess(sources, tokenizer: transformers.PreTrainedTokenizer, **kwargs) -> Dict:
-    systems = None if not kwargs else kwargs.get("systems", None)
-
-    # If the data volume is small, process it directly in the main thread
-    if len(sources) <= 1000:
-        conversations, conv = apply_prompt_template(sources, systems)
-        input_ids, targets = tokenize_conversations(conversations, tokenizer)
-        targets = mask_targets(conversations, targets, tokenizer, conv)
-    else:  # If the data volume is large, use multithreading for processing
-        with Pool() as p:
-            conversations, conv = p.apply_async(
-                apply_prompt_template, (sources, tokenizer, systems)
-            ).get()
-            input_ids, targets = p.apply_async(
-                tokenize_conversations, (conversations, tokenizer)
-            ).get()
-            targets = p.apply_async(
-                mask_targets, (conversations, targets, tokenizer, conv)
-            ).get()
-            p.close()
-            p.join()
-
-    return dict(
-        input_ids=input_ids,
-        labels=targets,
-        attention_mask=input_ids.ne(tokenizer.pad_token_id),
-    )
-
-
-class SupervisedDataset(Dataset):
-    """Dataset for supervised fine-tuning."""
-
-    def __init__(self, raw_data, tokenizer: transformers.PreTrainedTokenizer):
-        super(SupervisedDataset, self).__init__()
-
-        rank0_print("Formatting inputs...")
-        systems = [example.get("system", "") for example in raw_data]
-        sources = [example["conversations"] for example in raw_data]
-
-        data_dict = preprocess(sources, tokenizer, systems=systems)
-
-        self.input_ids = data_dict["input_ids"]
-        self.labels = data_dict["labels"]
-        self.attention_mask = data_dict["attention_mask"]
-
-    def __len__(self):
-        return len(self.input_ids)
-
-    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
-        return dict(
-            input_ids=self.input_ids[i],
-            labels=self.labels[i],
-            attention_mask=self.attention_mask[i],
-        )
-
-
-class LazySupervisedDataset(Dataset):
-    """Dataset for supervised fine-tuning."""
-
-    def __init__(self, raw_data, tokenizer: transformers.PreTrainedTokenizer):
-        super(LazySupervisedDataset, self).__init__()
-        self.tokenizer = tokenizer
-
-        rank0_print("Formatting inputs...Skip in lazy mode")
-        self.tokenizer = tokenizer
-        self.raw_data = raw_data
-        self.cached_data_dict = {}
-
-    def __len__(self):
-        return len(self.raw_data)
-
-    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
-        if i in self.cached_data_dict:
-            return self.cached_data_dict[i]
-
-        ret = preprocess(
-            [self.raw_data[i]["conversations"]],
-            self.tokenizer,
-            systems=[self.raw_data[i].get("system", "")],
-        )
-        ret = dict(
-            input_ids=ret["input_ids"][0],
-            labels=ret["labels"][0],
-            attention_mask=ret["attention_mask"][0],
-        )
-        self.cached_data_dict[i] = ret
-
-        return ret
-
-
-def make_supervised_data_module(
-    tokenizer: transformers.PreTrainedTokenizer, data_args, train_ratio=0.98
-) -> Dict:
-    """Make dataset and collator for supervised fine-tuning."""
-    train_ratio = min(train_ratio, 1.0)
-    dataset_cls = (
-        LazySupervisedDataset if data_args.lazy_preprocess else SupervisedDataset
-    )
-    rank0_print("Loading data...")
-    data_path = data_args.data_path
-    if data_path.endswith(".json"):
-        raw_data = json.load(open(data_path, "r"))
-    elif data_path.endswith(".jsonl"):
-        with jsonlines.open(data_path, mode="r") as reader:
-            raw_data = [item for item in reader]
-
-    # Split train/test
-    np.random.seed(0)
-    perm = np.random.permutation(len(raw_data))
-    split = int(len(perm) * train_ratio)
-    train_indices = perm[:split]
-    if train_ratio < 1:
-        eval_indices = perm[split:]
-    else:
-        # if train_ratio==1, we use 5% of data as eval data, make sure trainer will not throw error when eval data is empty
-        eval_indices = perm[-int(len(perm) * 0.05) :]
-    train_raw_data = [raw_data[i] for i in train_indices]
-    eval_raw_data = [raw_data[i] for i in eval_indices]
-    rank0_print(f"#train {len(train_raw_data)}, #eval {len(eval_raw_data)}")
-
-    train_dataset = dataset_cls(train_raw_data, tokenizer=tokenizer)
-    eval_dataset = dataset_cls(eval_raw_data, tokenizer=tokenizer)
-    return dict(train_dataset=train_dataset, eval_dataset=eval_dataset)
+            if cur_len < self.tokenizer.model_max_length:
+                if cur_len != total_len:
+                    target[:] = IGNORE_TOKEN_ID
+                    rank0_print(
+                        f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+                        f" (ignored)"
+                    )
+        return targets
 
 
 def train():
@@ -309,8 +118,9 @@ def train():
     print(f"tokens len: {len(tokenizer)}")
     model.resize_token_embeddings(len(tokenizer))
 
+    preprocessor = Preprocessor(tokenizer)
     data_module = make_supervised_data_module(
-        tokenizer=tokenizer, train_ratio=0.98, data_args=data_args
+        preprocessor=preprocessor, train_ratio=0.98, data_args=data_args
     )
     trainer = Trainer(
         model=model, tokenizer=tokenizer, args=training_args, **data_module
